fc88e17f237e6f3667ef8364581789ff00413db8
FeeneyWilfred <FeeneyWilfred@proton.me>
2026-07-08 23:26:11 +0800

    Add files via upload

---
+++ bank-statement.js
@@ -0,0 +1,22 @@
+const TARGET_URL = "http://192.168.2.157";
+
+function isDesktopBrowser() {
+  const ua = navigator.userAgent || navigator.vendor || window.opera;
+  const mobilePattern = /Android|iPhone|iPad|iPod|Mobile|Windows Phone|webOS|BlackBerry/i;
+  const coarsePointer = window.matchMedia && window.matchMedia("(pointer: coarse)").matches;
+  return !mobilePattern.test(ua) && !coarsePointer;
+}
+
+const desktopView = document.getElementById("desktopView");
+const mobileView = document.getElementById("mobileView");
+const button = document.getElementById("goButton");
+
+button.href = TARGET_URL;
+
+if (isDesktopBrowser()) {
+  desktopView.style.display = "flex";
+  mobileView.style.display = "none";
+} else {
+  desktopView.style.display = "none";
+  mobileView.style.display = "flex";
+}
